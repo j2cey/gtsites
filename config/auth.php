98141609ec7990cd3e9e771003db4bf43638f1fc
vagrant <vagrant@vagrant.vm>
2020-11-28 18:18:06 +0000

    LDAP Login + ldap guard

--- config/auth.php
+++ config/auth.php
@@ -46,6 +46,11 @@ return [
             'provider' => 'users',
             'hash' => false,
         ],
+
+        'ldap' => [
+            'driver' => 'session',
+            'provider' => 'ldap',
+        ],
     ],
 
     /*
